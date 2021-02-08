9b05ead53f7002b6aa79dc42d2b6625591e4ed3b
Mathias Wrobel <mathias@wrobel.dk>
2021-02-08 16:31:02 +0100

    Reverted to original deployment script

--- convex.ps1
+++ convex.ps1
@@ -4,11 +4,11 @@
 $ErrorActionPreference = 'stop'
 
 # Make sure RM is not installed
-if (Get-InstalledModule -name AzureRM -ErrorAction SilentlyContinue) {Write-Error "AzureRM cannot be installed for CONVEX to run. Please uninstall AzureRM and install the new Azure Module, AzureAD, Azure CLI, and Azure Function Core Tools."}
+if (Get-Module -name AzureRM -list) {Write-Error "AzureRM cannot be installed for CONVEX to run. Please uninstall AzureRM and install the new Azure Module, AzureAD, Azure CLI, and Azure Function Core Tools."}
 
 # Make sure the three required modules are installed
-if (-Not (Get-InstalledModule -name Azure -ErrorAction SilentlyContinue)) {Write-Error "Azure Module needs be installed for CONVEX to run. Please uninstall AzureRM and install the new Azure Module, AzureAD, Azure CLI, and Azure Function Core Tools."}
-if (-Not (Get-InstalledModule -name AzureAD -ErrorAction SilentlyContinue)) {Write-Error "AzureAD Module needs be installed for CONVEX to run. Please uninstall AzureRM and install the new Azure Module, AzureAD, Azure CLI, and Azure Function Core Tools."}
+if (-Not (Get-Module -name Azure -list)) {Write-Error "Azure Module needs be installed for CONVEX to run. Please uninstall AzureRM and install the new Azure Module, AzureAD, Azure CLI, and Azure Function Core Tools."}
+if (-Not (Get-Module -name AzureAD -list)) {Write-Error "AzureAD Module needs be installed for CONVEX to run. Please uninstall AzureRM and install the new Azure Module, AzureAD, Azure CLI, and Azure Function Core Tools."}
 if (-Not (az --version)) {Write-Error "AzureCLI needs be installed for CONVEX to run. Please uninstall AzureRM and install the new Azure Module, AzureAD, Azure CLI, and Azure Function Core Tools."}
 if (-Not (func --version)) {Write-Error "Azure Function Core Tools needs to be installed for CONVEX to run. Please uninstall AzureRM and install the new Azure Module, AzureAD, Azure CLI, and Azure Function Core Tools."}
 
@@ -21,21 +21,21 @@ $modules = $wd.ToString() + "\Utils\functions"
 Import-Module -Name $modules 
 
 # Connect to Azure Module
-<# try {
+try {
     Disconnect-AzAccount
     Connect-AzAccount
-} catch {Write-Error "Azure Powershell module must be installed and authenticated."} #>
+} catch {Write-Error "Azure Powershell module must be installed and authenticated."}
 
 # Connect to AzureAD
-<# try {
+try {
     Connect-AzureAD
-} catch {Write-Error "AzureAD PowerShell module must be installed and authenticated."} #>
+} catch {Write-Error "AzureAD PowerShell module must be installed and authenticated."}
 
 # Connect to AzureCLI
-<# try {
+try {
     az logout
    $login = az login --only-show-errors| ConvertFrom-Json
-} catch {Write-Error "Azure CLI must be installed and authenticated."} #>
+} catch {Write-Error "Azure CLI must be installed and authenticated."}
 
 
 # Print out available subs
@@ -51,9 +51,9 @@ catch{Write-Host "Error getting subscriptions from Az PowerShell Identity" -Fore
 # Get the first sub id and store the value
 do {
     $prompt1 = Read-Host -Prompt 'Input the Id of the start subscription'
-    #$input1 = "*" + $prompt1 + "*"
-    $SubOne = $allSubs | Where-Object id -CLike $prompt1
-    $s1 = $subIds.Match($SubOne.Id)
+    $input1 = "*" + $prompt1 + "*"
+    $SubOne = $allSubs | Where-Object id -CLike $input1
+    $s1 = $subIds.Contains($SubOne.Id)
     if (-Not $s1) {Write-Host "Not a valid subscription"}
 } until ($s1)
 
